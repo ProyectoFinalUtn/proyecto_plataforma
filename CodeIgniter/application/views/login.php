1edbff6a3c204217a6a49dedade856cab8c13127
Hernan Micelotta <u61139@inssjp.net>
2017-08-29 12:22:26 -0300

    Se agrega service rest para usuarios

--- CodeIgniter/application/views/login.php
+++ CodeIgniter/application/views/login.php
@@ -1,18 +1,12 @@
 <?php echo form_open('/sessions/authenticate') ?>
-<?php	$nombreUsuario = array('name' => 'nombreUsuario',
-	'placeholder' =>  '');
-	$password = array('name' =>  'password' ,
-	'placeholder' => '');
-
+<?php	
+    $nombreUsuario = array('name' => 'nombreUsuario', 'placeholder' =>  '');
+    $password = array('name' =>  'password', 'placeholder' => '');
 ?>
-<html>
-<body>
 <?php echo form_label('Usuario','usuario')?>
 <?php echo form_input($nombreUsuario)?>
 <?php echo form_label('Contraseña','password')?>
 <?php echo form_input($password)?>
 <?php echo form_submit('','Ingresar')?>
 <?php echo form_close()?>
-</body>
-</html>
 
