5eda5c847b10cb4073813f41906150830c21396b
hernan <>
2017-08-29 12:24:11 -0300

    Se modifica el controller de usuario

--- CodeIgniter/application/controllers/UsuarioVantController.php
+++ CodeIgniter/application/controllers/UsuarioVantController.php
@@ -14,7 +14,6 @@ class UsuarioVantController extends REST_Controller
         $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['user_by_id_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
-        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key*/
     }
 
     public function user_by_id_get()
