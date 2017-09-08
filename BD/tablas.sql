5f9e65a80f226176751cdcdc415cdbf7c0fe8f24
Hernan Mice <her_mice@yahoo.com.ar>
2017-09-07 23:03:30 -0300

    tablas

--- BD/tablas.sql
+++ BD/tablas.sql
@@ -1,6 +1,24 @@
+CREATE DATABASE goguide ;
+--connect goguide ;
+CREATE EXTENSION postgis;
+
+CREATE USER admin WITH PASSWORD 'proyecto2017';
+
+GRANT ALL PRIVILEGES ON DATABASE goguide to admin;
+
 -- Table: public.administrador
 
 -- DROP TABLE public.administrador;
+
+CREATE SEQUENCE public.admin_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.admin_seq
+    OWNER TO postgres;
 
 CREATE TABLE public.administrador
 (
@@ -32,8 +50,9 @@ CREATE TABLE public.menus
     id integer NOT NULL,
     name text COLLATE pg_catalog."default" NOT NULL,
     slug text COLLATE pg_catalog."default" NOT NULL,
+    icono text COLLATE pg_catalog."default",
     parent integer,
-    "number" integer NOT NULL,
+    orden integer NOT NULL,
     icon text COLLATE pg_catalog."default"
 )
 WITH (
@@ -52,11 +71,27 @@ GRANT ALL ON TABLE public.menus TO postgres;
 
 -- DROP TABLE public.perfil;
 
+
+
+-- Table: public.perfil
+
+-- DROP TABLE public.perfil;
+
+CREATE SEQUENCE public.perfil_idPerfil_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.perfil_idPerfil_seq
+    OWNER TO postgres;
+
 CREATE TABLE public.perfil
 (
-    "idPerfil" bigint NOT NULL DEFAULT nextval('"perfil_idPerfil_seq"'::regclass),
+    idPerfil bigint NOT NULL DEFAULT nextval('perfil_idPerfil_seq'::regclass),
     foto text COLLATE pg_catalog."default",
-    "logueadoEnCad" boolean NOT NULL,
+    logueadoEnCad boolean NOT NULL,
     CONSTRAINT perfil_pkey PRIMARY KEY ("idPerfil")
 )
 WITH (
@@ -66,18 +101,28 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.perfil
     OWNER to admin;
-	
+
 -- Table: public.persona
 
 -- DROP TABLE public.persona;
 
+CREATE SEQUENCE public.persona_idPersona_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.persona_idPersona_seq
+    OWNER TO postgres;
+
 CREATE TABLE public.persona
 (
-    "idPersona" bigint NOT NULL DEFAULT nextval('"persona_idPersona_seq"'::regclass),
+    idPersona bigint NOT NULL DEFAULT nextval('persona_idPersona_seq'::regclass),
     nombre text COLLATE pg_catalog."default" NOT NULL,
     apellido text COLLATE pg_catalog."default" NOT NULL,
-    "idTipoDocumento" integer,
-    "nroDocumento" integer,
+    idTipoDocumento integer,
+    nroDocumento integer,
     edad integer,
     sexo "char",
     calle text COLLATE pg_catalog."default",
@@ -97,17 +142,26 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.persona
     OWNER to admin;
-	
+
 -- Table: public.usuario_vant
 
 -- DROP TABLE public.usuario_vant;
+CREATE SEQUENCE public.UsuarioVant_idUsuario_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.UsuarioVant_idUsuario_seq
+    OWNER TO postgres;
 
 CREATE TABLE public.usuario_vant
 (
-    "idUsuario" bigint NOT NULL DEFAULT nextval('"UsuarioVant_idUsuario_seq"'::regclass),
-    "idRol" integer NOT NULL,
-    "idPersona" integer NOT NULL,
-    "idPerfil" integer,
+    idUsuario bigint NOT NULL DEFAULT nextval('UsuarioVant_idUsuario_seq'::regclass),
+    idRol integer NOT NULL,
+    idPersona integer NOT NULL,
+    idPerfil integer,
     usuario text COLLATE pg_catalog."default" NOT NULL,
     pass text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "UsuarioVant_pkey" PRIMARY KEY ("idUsuario")
@@ -119,3 +173,16 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.usuario_vant
     OWNER to admin;
+
+INSERT INTO administrador
+values    (1,'admin','202cb962ac59075b964b07152d234b70');
+
+INSERT INTO menus (id, parent, name, icono, slug, orden) VALUES
+(1, NULL, 'Normativa', '', 'normativas', 1),
+(2, NULL, 'Usuarios VANT', '', 'usuarios_vant', 2),
+(3, NULL, 'Solicitudes de Excepcion', '', 'solicitudes_excepcion', 3),
+(4, 1, 'Zonas de Influencia', '', 'zonas_influencia', 1),
+(5, 1, 'Cargar Zonas', '', 'cargar_zonas', 2),
+(6, 1, 'Zonas Temporales', '', 'zonas_temporales', 1),
+(7, 2, 'Listar Usuarios', '', 'listar_usuarios', 2),
+(8, 3, 'Listar Solicitudes', '', 'listar_solicitudes', 2);
