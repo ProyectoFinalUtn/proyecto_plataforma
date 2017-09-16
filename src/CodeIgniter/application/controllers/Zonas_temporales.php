77ec7cbcc085c0116bec54d2d12957c9c04d83f9
Gabo <fg.maze@gmail.com>
2017-09-16 14:14:53 -0300

    Nombre de archivos de controladores y vistas en mayuscula

---
+++ src/CodeIgniter/application/controllers/Zonas_temporales.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Zonas_influencia extends MY_Controller
+{
+
+	 public function __construct()
+    {
+        parent::__construct();        
+    }
+
+	public function index()
+	{	
+            $this->load->view('zonas_influencia'); 	 		 	 
+	}
+}
--- src/CodeIgniter/application/views/header.php
+++ src/CodeIgniter/application/views/header.php
@@ -13,17 +13,13 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-submenu.min.css">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/doc.min.css">
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
-   integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ=="
-   crossorigin=""/>
+
+
 
 
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js" defer></script>
 <script src="<?php echo base_url(); ?>assets/js/bootstrap-submenu.min.js" defer></script>
 <script src="<?php echo base_url(); ?>assets/js/docs.js" defer></script>
-<script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"
-   integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log=="
-   crossorigin=""></script>
 <script src="<?php echo base_url(); ?>assets/js/global.js" defer></script>
 
 
