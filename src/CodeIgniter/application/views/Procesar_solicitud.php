f5a1081100ceb29ec773431b91abf8c328ea0ce2
Esteban Campos <estebanluiscampos@gmail.com>
2017-10-04 11:17:35 -0300

    intento de agregar mapa

--- src/CodeIgniter/application/views/Procesar_solicitud.php
+++ src/CodeIgniter/application/views/Procesar_solicitud.php
@@ -3,9 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/ol.css">
+        <link href="//cdn.jsdelivr.net/openlayers.contextmenu/latest/ol3-contextmenu.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/ol3-layerswitcher.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
         <link rel="stylesheet" href="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+        
+        <script src="<?php echo base_url(); ?>assets/js/global.js" defer></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ol3/4.3.3/ol.js"></script>
+        <script src="//cdn.jsdelivr.net/openlayers.geocoder/latest/ol3-geocoder.js"></script>
+        <script src="//cdn.jsdelivr.net/openlayers.contextmenu/latest/ol3-contextmenu.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/olContextMenu.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/utility.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/ol3-layerswitcher.js"></script>
     </head>
     <style>
         h1,h2,h3,h4 { font-family: "Montserrat", sans-serif; }
@@ -13,41 +24,50 @@
         a { color: inherit;} 
         a:hover { text-decoration:none; }
         table { border-collapse: collapse; width: 100%; border-spacing:0; display:table; box-shadow:0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19); }
-        thead, th, td { text-align: left; padding: 8px; border-bottom: 1px solid #ddd; }
-        tr:hover { background-color: inherit; opacity: 0.7; }
+        td { padding: 8px ; }
     </style>
     <body>
         <div class = "main">
             <div id="breadcrumb" style="padding-left:8px;">G.O. Guide > Solicitudes de Excepción > Solicitud n°<?php echo $solicitud->idSolicitud; ?></div>
             <div style="padding:16px;">
                 <div id="detalle-solicitudes">
-                        <?php
-                            $estadosPosibles = array( 1 => "Pendiente", 2 => "Aprobada", 3 => "Rechazada", 4 => "Vencida" );
-                            echo "Número de solicitud ".$solicitud->idSolicitud."<br>";
-                            echo "Apellido: ".$solicitud->apellido."<br>";
-                            echo "Nombre: ".$solicitud->nombre."<br>";
-                            echo "Nro de Documento: ".$solicitud->documento."<br>";
-                            echo "Edad: ".$solicitud->edad."<br>";
-                            echo "E-mail: ".$solicitud->email."<br>";
-                            echo "Latitud: ".$solicitud->latitud."<br>";
-                            echo "Longitud".$solicitud->longitud."<br>";
-                            echo "Radio de vuelo: ".$solicitud->radioVuelo."<br>";
-                            echo "Fecha solicitada: ".$solicitud->fecha."<br>";
-                            echo "Horario solicitado: Desde las ".$solicitud->horaVueloDesde." Hasta las ".$solicitud->horaVueloHasta."<br>";
-                            echo "Solicitud procesada por: ".$solicitud->usuarioAprobador."<br>";
-                            echo "Estado actual de la solicitud: <select name=\"estados\">"."<option value=\"".$solicitud->idEstadoSolicitud."\" selected=\"selected\">" . 
-                                $solicitud->descripcionEstadoSolicitud."</option>";
-                            foreach ($estadosPosibles as $estado => $estadoPosible)
-                            {
-                                if (!($estado == $solicitud->idEstadoSolicitud))
-                                {
-                                    echo "<option value=\"".$estado."\">";
-                                    echo $estadoPosible;
-                                    echo "</option>";
-                                }
-                            }
-                            echo "</select><br><a href=\"#\">Procesar</a><br>";
-                        ?>
+                    <table>
+                        <tr>
+                            <td>
+                                <div id="map" class="map"></div>
+                                <script src="<?php echo base_url(); ?>assets/js/olTemporales.js"></script>
+                            </td>
+                            <td>
+                                <?php
+                                    $estadosPosibles = array( 1 => "Pendiente", 2 => "Aprobada", 3 => "Rechazada", 4 => "Vencida" );
+                                    echo "<strong>Número de solicitud:</strong> ".$solicitud->idSolicitud."<br>";
+                                    echo "<strong>Apellido:</strong> ".$solicitud->apellido."<br>";
+                                    echo "<strong>Nombre:</strong> ".$solicitud->nombre."<br>";
+                                    echo "<strong>Nro de Documento:</strong> ".$solicitud->documento."<br>";
+                                    echo "<strong>Edad:</strong> ".$solicitud->edad."<br>";
+                                    echo "<strong>E-mail:</strong> ".$solicitud->email."<br>";
+                                    echo "<strong>Latitud:</strong> ".$solicitud->latitud."<br>";
+                                    echo "<strong>Longitud:</strong> ".$solicitud->longitud."<br>";
+                                    echo "<strong>Radio de vuelo:</strong> ".$solicitud->radioVuelo."<br>";
+                                    echo "<strong>Fecha solicitada:</strong> ".$solicitud->fecha."<br>";
+                                    echo "<strong>Horario solicitado:</strong> Desde las ".$solicitud->horaVueloDesde." Hasta las ".$solicitud->horaVueloHasta."<br>";
+                                    echo "<strong>Solicitud procesada por:</strong> ".$solicitud->usuarioAprobador."<br>";
+                                    echo "<strong>Estado actual de la solicitud:</strong> <select name=\"estados\">"."<option value=\"".$solicitud->idEstadoSolicitud."\" selected=\"selected\">" . 
+                                        $solicitud->descripcionEstadoSolicitud."</option>";
+                                    foreach ($estadosPosibles as $estado => $estadoPosible)
+                                    {
+                                        if (!($estado == $solicitud->idEstadoSolicitud))
+                                        {
+                                            echo "<option value=\"".$estado."\">";
+                                            echo $estadoPosible;
+                                            echo "</option>";
+                                        }
+                                    }
+                                    echo "</select><br><a href=\"#\">Procesar</a><br>";
+                                ?>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
