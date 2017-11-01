b1ec81306fb28f35e288314882886e0ffb298c9e
Esteban Campos <estebanluiscampos@gmail.com>
2017-11-01 14:29:59 -0300

    Agrego nominatim al vuelo y fecha de vuelo

--- BD/bd_version_2.2.sql
+++ BD/bd_version_2.2.sql
@@ -3012,12 +3012,13 @@ ALTER SEQUENCE public.vuelo_id_vuelo_seq
 CREATE TABLE public.vuelo
 (
     id_vuelo bigint NOT NULL DEFAULT nextval('vuelo_id_vuelo_seq'::regclass),
-    id_usuario_vant bigint NOT NULL,
+    id_usuario_vant bigint,
     latitud text COLLATE pg_catalog."default" NOT NULL,
     longitud text COLLATE pg_catalog."default" NOT NULL,
     provincia text COLLATE pg_catalog."default",
     localidad text COLLATE pg_catalog."default",
     zona_interes text COLLATE pg_catalog."default",
+    fecha_vuelo date NOT NULL,
     CONSTRAINT vuelo_pkey PRIMARY KEY (id_vuelo),
     CONSTRAINT usuariovant_fkey FOREIGN KEY (id_usuario_vant)
         REFERENCES public.usuario_vant (id_usuario) MATCH SIMPLE
