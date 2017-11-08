10c2481eba5a0f64d9cbbc9100ac5b4d055eafbe
Gabo <fg.maze@gmail.com>
2017-11-08 16:39:29 -0300

    radio agregado a la consulta de zonas de influencia

--- src/CodeIgniter/application/models/Zonas_influencia_model.php
+++ src/CodeIgniter/application/models/Zonas_influencia_model.php
@@ -35,12 +35,12 @@ class Zonas_influencia_model extends CI_Model {
 
         public function get_zona_influencia($punto)
         {
-            $columnas = 'id, nombre_capa, geometria, detalle, radio';
+            $columnas = 'id, nombre_capa, geometria, detalle';
             $this->db->select($columnas);
             $this->db->from('zona_influencia');
             $where = '( ST_DWithin( 
                        ( ST_SetSRID(( ST_GeomFromGeoJSON (geometria::Text)),4326)),                  
-                       ( ST_SetSRID(ST_MakePoint('.strval($punto["long"]).','.strval($punto["lat"]).'),4326)),radio))';
+                       ( ST_SetSRID(ST_MakePoint('.strval($punto["long"]).','.strval($punto["lat"]).'),4326)),radio +'.strval($punto["rad"]).'))';
             $this->db->where($where);
             $query = $this->db->get();            
             return $query->result_array();
